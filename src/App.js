4a4ea07356c2b198bc7e71927de5f924792cb44d
Edward B <57622377+P4tt4te@users.noreply.github.com>
2023-02-07 17:22:55 +0100

    feat: add car & post processing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import { useRef, useState } from 'react'
-import { Canvas } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
+import * as THREE from 'three'
+import { useEffect, useRef, useState } from 'react'
+import { Canvas, useThree, useFrame } from '@react-three/fiber'
+import { OrbitControls, useGLTF } from '@react-three/drei'
+import { Selection, Select, EffectComposer, Outline } from '@react-three/postprocessing'
 import { BackSide, FrontSide } from 'three'
 
 function Box(props) {
@@ -25,7 +27,7 @@ function Box(props) {
   )
 }
 
-function Box2(props) {
+function BoxFront(props) {
   const ref = useRef()
   const [hovered, hover] = useState(null)
   console.log('cc!')
@@ -37,7 +39,16 @@ function Box2(props) {
   )
 }
 
+function Car(props) {
+  const group = props.ref
+  const { nodes, materials, scene } = useGLTF('assets/land_rover_defender/scene.gltf')
+
+  return <primitive ref={group} object={scene} dispose={null} />
+}
+
 export default function App() {
+  const carRef = useRef()
+
   return (
     <Canvas dpr={[1, 2]}>
       <OrbitControls />
@@ -45,9 +56,17 @@ export default function App() {
       <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
       <pointLight position={[-10, -10, -10]} />
       <group>
-        <Box2 />
+        <BoxFront />
         <Box scale={[1.03, 1.03, 1.03]} />
       </group>
+      <Selection>
+        <EffectComposer multisampling={16} autoClear={false}>
+          <Outline visibleEdgeColor="black" edgeStrength={10} width={500} height={500} xRay={false} />
+        </EffectComposer>
+        <Select enabled>
+          <Car ref={carRef} position={[0, 0, 0]} />
+        </Select>
+      </Selection>
     </Canvas>
   )
 }
