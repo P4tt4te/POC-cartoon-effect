79241d87444c947dc582c637f12e66e36b22a0b8
Edward B <57622377+P4tt4te@users.noreply.github.com>
2023-02-07 17:47:33 +0100

    feat: final config outline

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three'
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useRef, useState, forwardRef } from 'react'
 import { Canvas, useThree, useFrame } from '@react-three/fiber'
 import { OrbitControls, useGLTF } from '@react-three/drei'
 import { Selection, Select, EffectComposer, Outline } from '@react-three/postprocessing'
@@ -40,31 +40,28 @@ function BoxFront(props) {
 }
 
 function Car(props) {
-  const group = props.ref
+  const group = useRef()
   const { nodes, materials, scene } = useGLTF('assets/land_rover_defender/scene.gltf')
 
   return <primitive ref={group} object={scene} dispose={null} />
 }
 
 export default function App() {
-  const carRef = useRef()
-
   return (
     <Canvas dpr={[1, 2]}>
       <OrbitControls />
       <ambientLight intensity={0.5} />
       <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
       <pointLight position={[-10, -10, -10]} />
-      <group>
-        <BoxFront />
-        <Box scale={[1.03, 1.03, 1.03]} />
-      </group>
       <Selection>
         <EffectComposer multisampling={16} autoClear={false}>
-          <Outline visibleEdgeColor="black" edgeStrength={10} width={500} height={500} xRay={false} />
+          <Outline visibleEdgeColor="black" edgeStrength={3} width={1000} height={1000} xRay={false} />
         </EffectComposer>
         <Select enabled>
-          <Car ref={carRef} position={[0, 0, 0]} />
+          <Car position={[0, 0, 0]} />
+        </Select>
+        <Select enabled>
+          <BoxFront />
         </Select>
       </Selection>
     </Canvas>
